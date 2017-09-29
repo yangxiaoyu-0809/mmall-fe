ddb36c82cc35c09a36c89d779a51c4080b7691d5
yangxiaoyu-0809 <1150055925@qq.com>
2017-09-29 18:44:06 +0800

    beforeonline

---
+++ src/page/about/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: dell
+* @Date:   2017-09-29 17:02:26
+* @Last Modified by:   dell
+* @Last Modified time: 2017-09-29 17:03:20
+*/
+'use strict';
+
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 * @Author: dell
 * @Date:   2017-09-21 13:25:00
 * @Last Modified by:   dell
-* @Last Modified time: 2017-09-29 14:37:24
+* @Last Modified time: 2017-09-29 17:01:30
 */
 var webpack             = require('webpack');
 var ExtractTextPlugin   = require("extract-text-webpack-plugin");
@@ -18,6 +18,7 @@ var getHtmlConfig  = function(name, title){
     return{
     template    : './src/view/' + name + '.html',
     filename    : 'view/' + name + '.html',
+    favicon     : './favicon.ico',
     title       : title,
     inject      : true,
     hash        : true,
@@ -43,12 +44,13 @@ var config = {
         'user-center-update': ['./src/page/user-center-update/index.js'],
         'user-pass-update'  : ['./src/page/user-pass-update/index.js'],
         'result'            : ['./src/page/result/index.js'],
+        'about'             : ['./src/page/about/index.js'],
     },
     output: {
-        path:'./dist',
-        publicPath : '/dist',
-        filename: 'js/[name].js'
-    },
+        path        : __dirname + '/dist/',
+        publicPath  : 'dev' === WEBPACK_ENV ? '/dist/' : '//s.happymmall.com/mmall-fe/dist/',
+        filename    : 'js/[name].js'
+    },  
     externals : {
     	'jquery' : 'window.jQuery'
     },
@@ -56,7 +58,14 @@ var config = {
         loaders: [
         { test:/\.css$/, loader: ExtractTextPlugin.extract("style-loader","css-loader")},
         { test:/\.(gif|png|jpg|woff|svg|eot|ttf)\??.*$/, loader: 'url-loader?limit=100&name=resource/[name].[ext]'},
-        { test:/\.string$/, loader: 'html-loader'}
+        { 
+            test:/\.string$/, 
+            loader: 'html-loader',
+            query : {
+                minimize : true,
+                removeAttributeQuotes : false
+            }
+        }
         ]
     },
     resolve : {
@@ -92,6 +101,7 @@ var config = {
         new HtmlWebpackPlugin(getHtmlConfig('user-center-update', '修改个人信息')),
         new HtmlWebpackPlugin(getHtmlConfig('user-pass-update', '修改密码')),
         new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果')),
+        new HtmlWebpackPlugin(getHtmlConfig('about', '关于MMall')),
     ]
 };
 
